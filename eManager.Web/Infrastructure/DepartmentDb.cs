502c303799ba3ad04ca64e12e8987cce20a06d59
Taha Azab <azab.taha@gmail.com>
2012-11-19 15:53:13 +0200

    Added DepartmentDb context class

---
+++ eManager.Web/Infrastructure/DepartmentDb.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using eManager.Domains;
+
+namespace eManager.Web.Infrastructure
+{
+    public class DepartmentDb : DbContext, IDepartmentDataSource
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+
+        IQueryable<Employee> IDepartmentDataSource.Employees
+        {
+            get { return Employees; }
+        }
+
+        IQueryable<Department> IDepartmentDataSource.Departments
+        {
+            get { return Departments; }
+        }
+    }
+}
